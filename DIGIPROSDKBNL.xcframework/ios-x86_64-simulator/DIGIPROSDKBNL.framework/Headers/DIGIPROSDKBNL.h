29b60969c706abed3ab4b23bab6ff2fd8c07f485
Jonathan Viloria M <jonathanv@digipro.com.mx>
2020-11-24 21:52:19 -0600

    1.27.1.905

---
+++ DIGIPROSDKBNL.xcframework/ios-x86_64-simulator/DIGIPROSDKBNL.framework/Headers/DIGIPROSDKBNL.h
@@ -0,0 +1,19 @@
+//
+//  DIGIPROSDKBNL.h
+//  DIGIPROSDKBNL
+//
+//  Created by Desarrollo on 9/11/19.
+//  Copyright © 2019 Digipro. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for DIGIPROSDKBNL.
+FOUNDATION_EXPORT double DIGIPROSDKBNLVersionNumber;
+
+//! Project version string for DIGIPROSDKBNL.
+FOUNDATION_EXPORT const unsigned char DIGIPROSDKBNLVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <DIGIPROSDKBNL/PublicHeader.h>
+
+
